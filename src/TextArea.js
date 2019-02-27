da46b8554f32580e4519a85a3dc95fde3e503a63
Sameeha Rahman <SameehaR@99x.lk>
2019-02-27 16:15:04 +0530

    Create custom TextArea component

---
+++ src/TextArea.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { TextInput, StyleSheet, Keyboard, Platform } from "react-native";
+import PropTypes from "prop-types";
+
+export class TextArea extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputHeight: 50,
+            focused: false
+        }
+    }
+
+    render() {
+        const { onChangeText, placeholder, color, ...otherProps } = this.props;
+
+        const underLineColor = !color ? "#000" : color
+
+        if (Platform.OS === "android") {
+            return (
+                <TextInput
+                    ref={(c) => this._root = c}
+                    style={this.state.focused ? [styles.inputAndroid, { height: Math.max(50, this.state.inputHeight), borderBottomColor: underLineColor, borderBottomWidth: 2 }] : [styles.inputAndroid, { height: Math.max(50, this.state.inputHeight), borderBottomColor: underLineColor, borderBottomWidth: 1 }]}
+                    underlineColorAndroid={"transparent"}
+                    placeholder={placeholder}
+                    onChangeText={onChangeText}
+                    multiline
+                    onContentSizeChange={(event) => {
+                        this.setState({ inputHeight: event.nativeEvent.contentSize.height })
+                    }}
+                    onFocus={() => this.setState({ focused: true })}
+                    onBlur={() => this.setState({ focused: false })}
+                    {...otherProps}
+                />
+            )
+        }
+        return (
+            <TextInput
+                ref={(c) => this._root = c}
+                style={[styles.inputIos, { height: Math.max(50, this.state.inputHeight) }]}
+                underlineColorAndroid={"transparent"}
+                placeholder={placeholder}
+                onChangeText={onChangeText}
+                multiline
+                onContentSizeChange={(event) => {
+                    this.setState({ inputHeight: event.nativeEvent.contentSize.height })
+                }}
+                {...otherProps}
+            />
+        )
+    }
+}
+
+TextArea.propTypes = {
+    placeholder: PropTypes.string,
+    color: PropTypes.string,
+    onChangeText: PropTypes.func
+}
+
+const styles = StyleSheet.create({
+    inputAndroid: {
+        width: "100%",
+        borderTopRightRadius: 3,
+        borderTopLeftRadius: 3,
+        marginVertical: 10,
+    },
+    inputIos: {
+        backgroundColor: "white",
+        borderRadius: 10,
+        paddingHorizontal: 5,
+        marginVertical: 10,
+        width: '100%',
+        borderColor: "#e2e2e2",
+        borderWidth: 0.9
+    }
+})
