0d9b07db811c41019116ff57afe2d5c1813577a5
Sameeha Rahman <SameehaR@99x.lk>
2019-04-05 13:42:56 +0530

    Add unit test cases for Badge atom

---
+++ __tests__/atoms/Badge.android.js
@@ -0,0 +1,45 @@
+import "react-native";
+import React from "react";
+import renderer from "react-test-renderer";
+import { Badge } from "firstBorn/src/atoms/Badge";
+import { Text } from "../../src/atoms/Text";
+import { Icon } from "../../src/atoms/Icon";
+
+jest.mock("Platform", () => {
+    const Platform = require.requireActual("Platform");
+    Platform.OS = "android";
+    return Platform;
+});
+
+it("renders regular badge", () => {
+    const tree = renderer
+        .create(
+            <Badge>
+                <Text>+1</Text>
+            </Badge>
+        )
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders colored badge", () => {
+    const tree = renderer
+        .create(
+            <Badge color="red">
+                <Text>+1</Text>
+            </Badge>
+        )
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders icon badge", () => {
+    const tree = renderer
+        .create(
+            <Badge>
+                <Icon name="heart" />
+            </Badge>
+        )
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
