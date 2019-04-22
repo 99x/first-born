59ddf6022b30046ef826c641a0fdd3549f77736e
Sameeha Rahman <rahman.sameeha@gmail.com>
2019-04-22 13:53:07 +0530

    Create new component and manager for SnackBars

--- index.js
+++ index.js
@@ -25,9 +25,13 @@ export { ListItem } from "./src/molecules/listItem/ListItem"
 export { FloatingButton } from "./src/molecules/floatingButton/FloatingButton";
 
 // Notification molecules
-import NotificationManager from "./src/molecules/notifications/NotificationManager";
+import NotificationManager from "./src/molecules/notifications/simple/NotificationManager";
 export { NotificationManager as NotificationBarManager };
-export { Notification } from "./src/molecules/notifications/Notification";
+export { Notification } from "./src/molecules/notifications/simple/Notification";
+
+import SnackBarManager from "./src/molecules/notifications/snackbar/SnackBarManager";
+export { SnackBarManager as SnackManager };
+export { SnackBar } from "./src/molecules/notifications/snackbar/SnackBar";
 
 // Organisms
 
