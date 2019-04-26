b4bc7badb50f9324bedd844f01dbcda6e4c0348d
Sameeha Rahman <rahman.sameeha@gmail.com>
2019-04-26 12:25:08 +0530

    Update ListItem and Card image prop to accept all image propTypes

--- src/molecules/listItem/ListItem.js
+++ src/molecules/listItem/ListItem.js
@@ -51,8 +51,8 @@ export class ListItem extends Component {
         const onListItemTap = children
             ? this.expandListItem
             : onPress
-            ? onPress
-            : undefined;
+                ? onPress
+                : undefined;
 
         return (
             <View style={{ width: "100%" }}>
@@ -64,7 +64,7 @@ export class ListItem extends Component {
                 >
                     {image && (
                         <View style={styles.imageContainer}>
-                            <Image source={image} style={styles.image} />
+                            <Image style={styles.image} {...image} />
                         </View>
                     )}
                     <View style={styles.textContainer}>
@@ -89,8 +89,8 @@ export class ListItem extends Component {
                                             ? "arrow-up"
                                             : "arrow-dropup"
                                         : Platform.OS === "ios"
-                                        ? "arrow-down"
-                                        : "arrow-dropdown"
+                                            ? "arrow-down"
+                                            : "arrow-dropdown"
                                 }
                                 size={20}
                                 color={commonColors.inputGrey}
@@ -111,7 +111,9 @@ export class ListItem extends Component {
 ListItem.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string,
-    image: PropTypes.any,
+    image: PropTypes.shape({
+        ...Image.propTypes
+    }),
     block: PropTypes.bool,
     backgroundColor: PropTypes.string,
     secondary: PropTypes.bool,
