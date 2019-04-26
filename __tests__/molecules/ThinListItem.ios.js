f3a487f752c6c75bce77bb723d4f7167f07708ed
Sameeha Rahman <rahman.sameeha@gmail.com>
2019-04-26 15:45:42 +0530

    Create new unit test cases for ThinListItem and update ListView test case and snapshots

---
+++ __tests__/molecules/ThinListItem.ios.js
@@ -0,0 +1,44 @@
+import "react-native";
+import React from "react";
+import renderer from "react-test-renderer";
+import { ThinListItem } from "../../src/molecules/listItem/ThinListItem";
+
+jest.mock("Platform", () => {
+    const Platform = require.requireActual("Platform");
+    Platform.OS = "ios";
+    return Platform;
+});
+
+it("renders thin list item with heading", () => {
+    const tree = renderer.create(<ThinListItem title="Heading" />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders thin list item with heading and title", () => {
+    const tree = renderer
+        .create(<ThinListItem title="Heading" description="description" />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders thin list item with heading, title and arrow", () => {
+    const tree = renderer
+        .create(
+            <ThinListItem title="Heading" description="description" arrow />
+        )
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders colored thin list item with heading and title", () => {
+    const tree = renderer
+        .create(
+            <ThinListItem
+                title="Heading"
+                description="description"
+                color="aliceblue "
+            />
+        )
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
