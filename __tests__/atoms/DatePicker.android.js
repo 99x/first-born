7f237bcf94eea6cac4013895e6ba892221ab8229
Sameeha Rahman <SameehaR@99x.lk>
2019-05-30 13:41:39 +0530

    Update unit test cases and snapshots

--- __tests__/atoms/DatePicker.android.js
+++ __tests__/atoms/DatePicker.android.js
@@ -14,6 +14,37 @@ it("renders regular DatePicker", () => {
     expect(tree).toMatchSnapshot();
 });
 
+it("renders underlined DatePicker", () => {
+    const tree = renderer.create(<DatePicker underline />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders rounded DatePicker", () => {
+    const tree = renderer.create(<DatePicker rounded />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders unstyled DatePicker", () => {
+    const tree = renderer.create(<DatePicker noStyle />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it("renders custom styled DatePicker", () => {
+    const tree = renderer
+        .create(
+            <DatePicker
+                noStyle
+                style={{
+                    borderWidth: 1,
+                    borderColor: "black",
+                    borderRadius: "3"
+                }}
+            />
+        )
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
 it("renders colored DatePicker", () => {
     const tree = renderer.create(<DatePicker color="red" />).toJSON();
     expect(tree).toMatchSnapshot();
